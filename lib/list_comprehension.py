1e26df7960c02454240403022f11af044e444689
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-23 17:27:47 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    for n in num_list:
+        if n %2 !=0:
+            return []
+        else:
+            return [n for n in num_list if n %2==0 ]
+      
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list==[]:
+        return []
+    else:
+        return [(x + "!") for x in sentence_list]
